ce6fdf4b6eb7adec7a380f3cdb21c15b2c2daf14
Eric Magalhães <15831837+eric-souzams@users.noreply.github.com>
2021-08-21 21:04:05 -0300

    Create project

---
+++ data.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE dev_sices;
+
+USE dev_sices;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(80) UNIQUE NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    is_admin BOOLEAN NOT NULL DEFAULT false
+);
+CREATE TABLE IF NOT EXISTS working_hours (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    user_id INT,
+    work_date DATE NOT NULL,
+    time1 TIME,
+    time2 TIME,
+    time3 TIME,
+    time4 TIME,
+    worked_time INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT cons_user_day UNIQUE (user_id, work_date)
+);
+
+-- Essa senha criptografada corresponde ao valor "a"
+INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
+VALUES (1, 'Admin', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'admin@gmail.com.br', '2020-5-1', null, 1);
+
+INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
+VALUES (2, 'Lucas Santos', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'lucas@gmail.com.br', '2020-1-1', null, 0);
+
+INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
+VALUES (3, 'Pedro Souza', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'pedro@gmail.com.br', '2020-1-1', null, 0);
+
+INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
+VALUES (4, 'Eric Magalhães', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'eric@gmail.com.br', '2020-1-1', null, 0);
+
+INSERT INTO users (id, name, password, email, start_date, end_date, is_admin)
+VALUES (5, 'Vinicius Martins', '$2y$10$/vC1UKrEJQUZLN2iM3U9re/4DQP74sXCOVXlYXe/j9zuv1/MHD4o.', 'vinicius@gmail.com.br', '2020-1-1', '2019-1-1', 0);
